eb7ed637f5802c3b096855448ecb904011dd5a2a
eunjikangi <miki2323@naver.com>
2024-07-18 06:39:19 +0900

    Ch10-29. 스크롤 위치 계산 애니메이션 & Rotate 3D 애니메이션

--- js/main.js
+++ js/main.js
@@ -113,3 +113,14 @@ floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
 
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      // View Port 전체 : 1
+      triggerHook: .8,
+    })
+    .setClassToggle()
+    .addTo();
+});
